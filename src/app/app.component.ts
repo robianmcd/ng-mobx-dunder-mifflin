c7ea7f5a8f8c8b48653b15569ac9e8c752a19f29
Rob McDiarmid <rmcdiarmid@konradgroup.com>
2017-11-20 18:05:50 -0500

    changed async action to use onHasTask

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import EmployeeStore from './services/stores/employeeStore.service';
 import Employee from './models/employee.model';
 
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+
 @Component({
   selector: 'app-root',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -12,7 +15,7 @@ import Employee from './models/employee.model';
       <div *ngFor="let employee of employeeStore.employees">
         {{employee.name}} - {{getChildrenString(employee)}}
       </div>
-      
+
       <div *ngIf="employeeStore.boss">Boss man: {{employeeStore.boss.name}}</div>
     </div>
   `,
@@ -30,3 +33,32 @@ export class AppComponent {
   }
 
 }
+
+declare const Zone;
+
+
+// let myZone = Zone.current.fork({
+//   onScheduleTask: (parentZoneDelegate, currentZone, targetZone, task) => {
+//     console.log('scheduling', task);
+//     return parentZoneDelegate.scheduleTask(targetZone, task);
+//   },
+//   onInvokeTask(delegate, currentZone, targetZone, task, ...args) {
+//     const result = delegate.invokeTask(targetZone, task, ...args);
+//     const name = task.callback.name;
+//     console.log('invokink name');
+//   },
+//   onInvoke(delegate, current, target, callback, ...args) {
+//     console.log(`onInvoke '${target.name}'`);
+//     return delegate.invoke(target, callback, ...args);
+//   }
+// });
+//
+// myZone.run(() => {
+//   console.log('task: 1');
+//   Observable
+//     .interval(1000)
+//     .subscribe((val) => {
+//         console.log(val);
+//     });
+//
+// });
--- src/app/asyncAction.ts
+++ src/app/asyncAction.ts
@@ -1,24 +1,68 @@
-import {action, runInAction} from 'mobx';
+import {action, endAction, runInAction, startAction} from 'mobx';
 
 declare const Zone;
 
-const asyncAction = function(target, prop: string, descriptor) {
+const asyncAction = function (target, prop: string, descriptor) {
+  let pendingAction;
+  let finishedInitialSyncAction = false;
+
   //Create a zone that will wrap all async tasks in an action
   let actionZone = Zone.current.fork({
-    onInvoke: (parentZoneDelegate, currentZone, targetZone, delegate: Function,
-               applyThis: any, applyArgs: any[], source: string) => {
+    name: `mobxActionZone:${prop}`,
+    // onInvoke: (parentZoneDelegate, currentZone, targetZone, delegate: Function,
+    //            applyThis: any, applyArgs: any[], source: string) => {
+    //
+    //   let actionName = `${prop}:${delegate.name || 'async'}`;
+    //   let actionDelegate = (...args) => runInAction(actionName, () => delegate(...args));
+    //
+    //   return parentZoneDelegate.invoke(targetZone, actionDelegate, applyThis, applyArgs, source);
+    // }
 
-      let actionName = `${prop}:${delegate.name || 'async'}`;
-      let actionDelegate = (...args) => runInAction(actionName, () => delegate(...args));
+    // onInvoke(delegate, current, targetZone, callback, ...args) {
+    //   let actionName = `${prop}:${callback.name || 'async'}`;
+    //   return runInAction(actionName, () => delegate.invoke(targetZone, callback, ...args));
+    // }
 
-      return parentZoneDelegate.invoke(targetZone, actionDelegate, applyThis, applyArgs, source);
+    onScheduleTask(delegate, currentZone, targetZone, task) {
+      console.log('SCHEDULING TASK', task.type);
+      return delegate.scheduleTask(targetZone, task);
+    },
+
+    onInvokeTask(delegate, currentZone, targetZone, task, ...args) {
+      let actionName = `${prop}:${task.callback.name || 'async'}`;
+      console.log('INVOKE TASK', task.type, task.callback.name);
+      return delegate.invokeTask(targetZone, task, ...args);
+      //return runInAction(actionName, () => delegate.invokeTask(targetZone, task, ...args));
+    },
+
+    // onIntercept(parentZoneDelegate, currentZone, targetZone, delegate: Function, source: string) {
+    //   console.log('what the fork?');
+    // },
+    onHasTask(parentZoneDelegate, currentZone, targetZone, hasTaskState) {
+      console.log('HAS TASK', hasTaskState);
+
+      if (hasTaskState.change === 'microTask' && finishedInitialSyncAction) {
+        if (hasTaskState.microTask) {
+          pendingAction = startAction(`${prop}:async`, () => {}, null);
+        } else if (pendingAction) {
+          endAction(pendingAction);
+          pendingAction = undefined;
+        }
+      }
+
     }
   });
 
+
   //Modify the annotated function so that it runs in the actionZone
   let origFunc = descriptor.value;
-  descriptor.value = function(...args) {
-    return actionZone.run(() => origFunc.call(this, ...args));
+  descriptor.value = function (...args) {
+    return actionZone.run(() => {
+      finishedInitialSyncAction = false;
+      let result = origFunc.call(this, ...args);
+      finishedInitialSyncAction = true;
+      return result;
+    });
   };
 
   return action(target, prop, descriptor);
